bac0f60a7b2c592453f32594bdf7c1dda2cd6bb0
kingmoc <joe.c.a.garcia@gmail.com>
2019-07-09 13:28:17 -0500

    Display Added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Numbers from './components/ButtonComponents/NumberButtons/Numbers'
 import Logo from "./components/DisplayComponents/Logo";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Display from "./components/DisplayComponents/Display";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -19,6 +20,7 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <Display />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <div>
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -6,7 +6,7 @@ const NumberButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className="calcButton">{props.num}</button>
+      <button className={props.num === '0' ? 'calcButton zero' : 'calcButton'}>{props.num}</button>
     </>
   );
 };
